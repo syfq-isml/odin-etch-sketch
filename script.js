e4be0a092976c0ffbe5f346bf7adaa83c542cabe
syfq-isml <macamyesjer@gmail.com>
2022-03-01 22:53:39 +0800

    make createBoard function to create number of pixels on screen

--- script.js
+++ script.js
@@ -6,21 +6,23 @@ let squareWidth16 = divisions/16;
 
 
 let noOfSquares = 16;
+createBoard();
 
-for (let i=0; i<noOfSquares; i++) {
-    for (let j=0; j < noOfSquares; j++) {
-        let drawingBrdSq = document.createElement('div');
-        drawingBrdSq.className = "drawing-squares"
-       drawingBrdSq.style.boxSizing = "border-box";
-        drawingBrdSq.style.border = 'solid 1px black';
-        drawingBrdSq.style.width = `${squareWidth16}px`;
-        drawingBrdSq.style.height = `${squareWidth16}px`;
-        drawingBrdSq.style.padding = '0';
-        drawingBrdSq.style.margin = '0';
-        container.appendChild(drawingBrdSq);
+function createBoard() {
+    for (let i=0; i<noOfSquares; i++) {
+        for (let j=0; j < noOfSquares; j++) {
+            let drawingBrdSq = document.createElement('div');
+            drawingBrdSq.className = "drawing-squares"
+        drawingBrdSq.style.boxSizing = "border-box";
+            drawingBrdSq.style.border = 'solid 1px black';
+            drawingBrdSq.style.width = `${squareWidth16}px`;
+            drawingBrdSq.style.height = `${squareWidth16}px`;
+            drawingBrdSq.style.padding = '0';
+            drawingBrdSq.style.margin = '0';
+            container.appendChild(drawingBrdSq);
+        }
     }
 }
-
 
 let drawingBrdSq = document.getElementsByClassName('drawing-squares');
 for (let i =0; i < drawingBrdSq.length; i++) {
@@ -31,6 +33,12 @@ function colorSq(e) {
     e.target.className = `${e.target.className} hovering`;
 }
 
+let clrBtn = document.getElementById('clear-button');
+clrBtn.addEventListener('click', clearBoard);
+
+function clearBoard() {
+    
+}
 
 
 
